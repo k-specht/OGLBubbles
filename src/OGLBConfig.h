89622df20ddec454cca33e4e59543d98bc37cb45
k-specht <xpecht@gmail.com>
2020-09-14 13:36:56 -0400

    Fixed compiler errors - if using VSC, make sure to load VSC with a x64 command line window so cl.exe is x64

---
+++ src/OGLBConfig.h
@@ -0,0 +1,3 @@
+// Configured options and settings for the OGLBubbles project
+#define OGLBubbles_VERSION_MAJOR 
+#define OGLBubbles_VERSION_MINOR 
--- src/OGLBubbles.cpp
+++ src/OGLBubbles.cpp
@@ -1,24 +1,34 @@
-#define GLFW_DLL
-#define GLFW_INCLUDE_NONE
-
-#include "../include/glad/glad.h"
-#include "../include/GLFW/glfw3.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include "OGLBubbles.h"
 
 #include <iostream>
 
-#pragma comment(lib,".\\lib\\GLFW\\glfw3dll.lib")
+// Current issue: exit code 0xc0000135
+// If adding the library to the command line linker isn't your thing, uncomment this:
+//#pragma comment(lib,"glfw3dll.lib")
 
 using namespace std;
 
 int main()
 {
+    // Initialize
     std::cout << "Hello World!";
-    //glfwInit();
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  
+
+    // Create the window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
     return 0;
 }
