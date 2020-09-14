8029050b2a95f576b735d386e127cd48f4d73a87
k-specht <xpecht@gmail.com>
2020-09-14 15:41:41 -0400

    Put glad.c back in its rightful place and added it to the compile list.

--- src/OGLBubbles.cpp
+++ src/OGLBubbles.cpp
@@ -1,5 +1,5 @@
 #define GLFW_DLL
-#define GLFW_INCLUDE_NONE
+
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -7,6 +7,17 @@
 #include "OGLBubbles.h"
 
 #include <iostream>
+
+/**
+ *  Updates the viewport to the new size of the window's frame.
+ *  @param window - The window whose frame changed.
+ *  @param width  - The width of the new frame size.
+ *  @param height - The height of the new frame size.
+ */
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
 /**
  *  Entry point to the app.
@@ -23,7 +34,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    // Create the window
+    // Create the window and sets it as the current context
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -32,6 +43,31 @@ int main()
         return -1;
     }
     glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    } 
+
+    // Sets the default viewport size
+    glViewport(0, 0, 800, 600);
+
+    // When the frame size changes, this calls a function to update it
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
+
     
+    // Loops while the window is open so graphics keep being drawn
+    while(!glfwWindowShouldClose(window))
+    {
+        // Swaps the front and back buffers
+        glfwSwapBuffers(window);
+
+        // Processes any pending events, like mouse movement
+        glfwPollEvents();    
+    }
+
+    // Clean up all drawing resources before exiting the program
+    glfwTerminate();
     return 0;
 }
